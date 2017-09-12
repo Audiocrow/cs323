5a79c28b808575eed93d48e9beb16dff24e8324a
Audiocrow <mrcrowk@gmail.com>
2017-09-11 18:57:47 -0700

    main loop added

--- assignment3/main.cpp
+++ assignment3/main.cpp
@@ -1,1 +1,18 @@
-//An empty .cpp so I could make the git folder
+// Member Names:
+// Alexander Edgar
+// Daniel Domingo
+// Sean Wulwick
+
+int main() {
+	char reservedWords[4][10] = { "cout<<", "for", "int", "while" };
+	char special[8][3] = { "=", "*", "-", ";", "(", ")", "<=", "+" };
+	regex expr("(.*?)(?:\s|$)"); //Anything followed by a space or endline
+	while(1) {
+		cout << "Continue(y/n)?";
+		char c;
+		cin >> c;
+		cin.ignore();
+		if(c != 'y' && c != 'Y') { break; }
+	}
+	return 0;
+}
