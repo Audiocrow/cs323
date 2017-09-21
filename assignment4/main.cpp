600d8eead2fe3ec067395039f2cdb13593c8dea9
Audiocrow <mrcrowk@gmail.com>
2017-09-20 22:56:02 -0700

    fixed the token expression again and fixed the out of bounds exceptions

--- assignment4/main.cpp
+++ assignment4/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
 	ifstream ifile("./data.txt");
 	ofstream ofile("./newdata.txt");
-	regex token("(int|;|,|=|\\+|[a-zA-Z_])\\w+");
+	regex token("(\/\/|int|;|,|=|\\+|[a-zA-Z_]\\w*)");
 	string line, temp;
 	smatch match, tokMatch;
 	bool firstPrint=false, comment=false;
@@ -17,13 +17,15 @@ int main() {
 		cout <<"DEBUG: Read '" << line << "'" << endl;
 		while(regex_search(line, match, regex("(\\S+)(;)?"))) {
 			cout << "DEBUG: Matched '" << match.str(1) << "'";
-			if(match.size() > 2) cout << " and '" << match.str(3) << "'";
+			if(match.size() > 2 && match.str(2).length() > 0)
+				cout << " and '" << match.str(2) << "'";
 			cout << endl;
 			//Break up different tokens
 			firstPrint=false;
 			comment=false;
 			temp = match.str(1);
 			while(regex_search(temp, tokMatch, token)) {
+				cout << "\tDEBUG: Matched '" << tokMatch.str(1) << "'" << endl;
 				//Skip comments
 				if(tokMatch.str(1).length() > 1 && tokMatch.str(1).at(0) == '/' && tokMatch.str(1).at(1) == '/') {
 					comment=true;
@@ -41,7 +43,7 @@ int main() {
 			if(!firstPrint)
 				ofile << match.str(1);
 			//Rather than a space, ; is followed by newline
-			if(match.size() > 2 && match.str(2).at(0) == ';')
+			if(match.size() > 2 && match.str(2).length() > 0 && match.str(2).at(0) == ';')
 				ofile << endl;
 			line = match.suffix().str();
 		}
