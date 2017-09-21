0e3c35c4881d38e3c3370afe9690f75bb0a2a692
Audiocrow <mrcrowk@gmail.com>
2017-09-20 19:38:05 -0700

    Janky in-progress fix to detecting separate tokens. About to try a totally new approach instead

--- assignment4/main.cpp
+++ assignment4/main.cpp
@@ -9,17 +9,29 @@ using namespace std;
 int main() {
 	ifstream ifile("data.txt");
 	ofstream ofile("newdata.txt");
+	regex identifier("([a-zA-Z_]\w*)");
+	regex symbol("(int|;|,|=|+)");
 	string line;
-	smatch match;
+	smatch match, idMatch, symMatch;
 	while(file.good()) {
 		getline(ifile, line);
-		//Skip commented lines
-		if(line.at(0) == '/' && line.at(1) == '/')
-			continue;
-		while(regex_search(line, match, regex("(\S+)(\s+)"))) {
-			ofile << match.str(1);
+		while(regex_search(line, match, regex("(\S+)(\s+|//)"))) {
+			//Skip comments
+			if(match.str(1).compare("//") == 0)
+				break;
+			//Break up the word we matched by separate tokens - identifiers, symbols, etc.
+			bool matched = false;
+			while(regex_search(match.str(1), idMatch, identifier)) {
+				ofile << idMatch.str(1);
+				matched = true;
+			}
+			if(!matched)
+				ofile << match.str(1);
 			if(match.str(2).compare("\n") == 0 || match.str(2).compare("\r") == 0 || match.str(2).compare("\n\r") == 0 || match.str(2).compare("\r\n") == 0)
 				ofile << endl;
+			//Skip comments
+			else if(match.str(2).compare("//") == 0)
+				break;
 			else ofile << " ";
 			match = match.suffix().str();
 		}
