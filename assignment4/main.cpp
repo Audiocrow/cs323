c0ab552f7e6333fbc3f8792126204cf69cd6582d
Audiocrow <mrcrowk@gmail.com>
2017-09-20 19:22:42 -0700

    Untested possible solution

--- assignment4/main.cpp
+++ assignment4/main.cpp
@@ -1,1 +1,31 @@
 //dummy main to make new folder
+#include <fstream>
+#include <iostream>
+#include <regex>
+
+
+using namespace std;
+
+int main() {
+	ifstream ifile("data.txt");
+	ofstream ofile("newdata.txt");
+	string line;
+	smatch match;
+	while(file.good()) {
+		getline(ifile, line);
+		//Skip commented lines
+		if(line.at(0) == '/' && line.at(1) == '/')
+			continue;
+		while(regex_search(line, match, regex("(\S+)(\s+)"))) {
+			ofile << match.str(1);
+			if(match.str(2).compare("\n") == 0 || match.str(2).compare("\r") == 0 || match.str(2).compare("\n\r") == 0 || match.str(2).compare("\r\n") == 0)
+				ofile << endl;
+			else ofile << " ";
+			match = match.suffix().str();
+		}
+		line.clear();
+	}
+	ifile.close();
+	ofile.close();
+	return 0;
+}
