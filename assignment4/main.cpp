ed522a5716eb67406ec61e1188b5bd0b13249118
Audiocrow <mrcrowk@gmail.com>
2017-09-20 23:38:03 -0700

    Almost done. I have decided the nested regex block is unnecessary and I should be able to change it to just one regex block...

--- assignment4/main.cpp
+++ assignment4/main.cpp
@@ -8,43 +8,43 @@ using namespace std;
 int main() {
 	ifstream ifile("./data.txt");
 	ofstream ofile("./newdata.txt");
-	regex token("(\/\/|int|;|,|=|\\+|[a-zA-Z_]\\w*)");
+	regex token("(//|int|;|,|=|\\+|\\d+(?:.\\d+)?|[a-zA-Z_]\\w*)");
 	string line, temp;
 	smatch match, tokMatch;
-	bool firstPrint=false, comment=false;
+	bool shouldSpace=false, comment=false, foundType=false;
 	while(ifile.good()) {
 		getline(ifile, line);
 		cout <<"DEBUG: Read '" << line << "'" << endl;
-		while(regex_search(line, match, regex("(\\S+)(;)?"))) {
-			cout << "DEBUG: Matched '" << match.str(1) << "'";
-			if(match.size() > 2 && match.str(2).length() > 0)
-				cout << " and '" << match.str(2) << "'";
-			cout << endl;
+		while(regex_search(line, match, regex("(\\S+)"))) {
+			cout << "DEBUG: Matched '" << match.str(1) << "'" << endl;
 			//Break up different tokens
-			firstPrint=false;
+			shouldSpace=false;
 			comment=false;
+			foundType = true;
 			temp = match.str(1);
 			while(regex_search(temp, tokMatch, token)) {
+				foundType = true;
 				cout << "\tDEBUG: Matched '" << tokMatch.str(1) << "'" << endl;
 				//Skip comments
 				if(tokMatch.str(1).length() > 1 && tokMatch.str(1).at(0) == '/' && tokMatch.str(1).at(1) == '/') {
 					comment=true;
 					break;
 				}
-				if(!firstPrint)
+				if(shouldSpace)
 					ofile << " ";
 				ofile << tokMatch.str(1);
-				firstPrint = true;
+				if(tokMatch.str(1).compare(";") == 0) {
+					ofile << endl;
+					shouldSpace = false;
+				}
+				else shouldSpace = true;
 				temp = tokMatch.suffix().str();
 			}
 			if(comment)
 				break;
-			//Something unknown was found, print it as its own token
-			if(!firstPrint)
+			//Something not specified in tokens was found, so print it anyway
+			if(!foundType)
 				ofile << match.str(1);
-			//Rather than a space, ; is followed by newline
-			if(match.size() > 2 && match.str(2).length() > 0 && match.str(2).at(0) == ';')
-				ofile << endl;
 			line = match.suffix().str();
 		}
 		//ofile << endline;
