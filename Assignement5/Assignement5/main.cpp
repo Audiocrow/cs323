b9d4fc8d8b00bc5730cfc766addcb8e295e9598b
SeaLabEZF <sean.wulwick@gmail.com>
2017-09-25 15:27:43 -0700

    Initial build
    Complete and ready for testing.

--- Assignement5/Assignement5/main.cpp
+++ Assignement5/Assignement5/main.cpp
@@ -1,1 +1,40 @@
-//main created to commit.
+//Domingo, Daniel
+//Edgar, Alexander
+//Wulwick, Sean
+#include <iostream>
+#include <regex>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+	regex expr("(a*)(a|b)(c*)\\$");
+	string token;
+	char repeat = 'y';
+	bool ever = true;
+	
+	for (; ever;) {
+		cout << "Please give a language to compare (complete with '$') ";
+		cin >> token;
+		if (token.back() != '$') {
+			cout << "Invalid expression please retype." << endl;
+		}
+		else {
+			if (regex_match(token, expr)) {
+				cout << "token exists in language." << endl
+					<< "try again?(y/n) ";
+
+			}
+			else {
+				cout << "token does not exist in language." << endl
+					<< "try again?(y/n) ";
+			}
+			cin >> repeat;
+			cout << endl;
+			repeat = tolower(repeat);
+			if (repeat == 'n')
+				return 0;
+		}
+	}
+}
