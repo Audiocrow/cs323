cfa94193c274ed6719c8f2428592575981dd0362
SeaLabEZF <sean.wulwick@gmail.com>
2017-09-25 15:37:08 -0700

    small updates
    Error checking mostly

--- Assignement5/Assignement5/main.cpp
+++ Assignement5/Assignement5/main.cpp
@@ -13,28 +13,34 @@ int main() {
 	string token;
 	char repeat = 'y';
 	bool ever = true;
-	
+
 	for (; ever;) {
+
+		//asks for user input of language to compare
 		cout << "Please give a language to compare (complete with '$') ";
 		cin >> token;
+
+		//checks for '$' ending statement in language
 		if (token.back() != '$') {
-			cout << "Invalid expression please retype." << endl;
+			cout << "Invalid expression please retype." << endl << endl;
 		}
 		else {
-			if (regex_match(token, expr)) {
-				cout << "token exists in language." << endl
-					<< "try again?(y/n) ";
+			//compare statement on token
+			if (regex_match(token, expr))
+				cout << "token exists in language." << endl;
+			else
+				cout << "token does not exist in language." << endl;
 
-			}
-			else {
-				cout << "token does not exist in language." << endl
-					<< "try again?(y/n) ";
-			}
-			cin >> repeat;
-			cout << endl;
-			repeat = tolower(repeat);
-			if (repeat == 'n')
-				return 0;
+			//ask user if they would like to continue
+			do {
+				cout << "try again?(y/n) ";
+				cin >> repeat;
+				cout << endl;
+
+				repeat = tolower(repeat);
+				if (repeat == 'n')
+					return 0;
+			} while (repeat != 'y' && repeat != 'n');
 		}
 	}
 }
