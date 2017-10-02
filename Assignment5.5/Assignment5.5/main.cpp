b462d68aaf36944b3a2b30426184fa613314fe96
SeaLabEZF <sean.wulwick@gmail.com>
2017-10-02 13:18:08 -0700

    had like 30 seconds
    lol

--- Assignment5.5/Assignment5.5/main.cpp
+++ Assignment5.5/Assignment5.5/main.cpp
@@ -1,1 +1,12 @@
 //empty project for new assignment due 5 OCT 2017
+//Domingo, Daniel
+//Edgar, Alexander
+//Wulwick, Sean
+
+#include <iostream>
+#include <regex>
+#include <map>
+
+using namespace std;
+
+
