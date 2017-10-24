d944cd88173e25191fffcefd237b8cb9bc20fb15
Audiocrow <mrcrowk@gmail.com>
2017-10-23 18:56:03 -0700

    working and correct(?).. might wanna change what error # is printed, currently its the number of the box in the table but instead it might need to count only empty boxes?

--- CPSC323-Assignment8/CPSC323-Assignment8/main.cpp
+++ CPSC323-Assignment8/CPSC323-Assignment8/main.cpp
@@ -1,6 +1,7 @@
+#include <algorithm>
 #include <iostream>
 #include <map>
-#inclde <stack>
+#include <stack>
 #include <string>
 
 using namespace std;
@@ -16,34 +17,39 @@ string printStack(stack<char>& myStack) {
 	return result;
 }
 
-//Returns "success" if the word could be traced
-//Otherwise returns the number of the failed spot on the table
-string traceWord(string word, string& parsingTable, char firstState, map<char,int>& states, map<char,int>& terminals) {
+//Returns true if the word could be traced and is valid, otherwise false
+template <size_t rows, size_t cols>
+bool traceWord(string word, string (&parsingTable)[rows][cols], char firstState, map<char,int>& states, map<char,int>& terminals) {
 	int state;
 	stack<char> traceStack;
 	char top;
-	map::iterator<char,int> mapIt;
+	map<char,int>::iterator mapIt;
 	//Pop traceStack and return what was popped
-	char pop = [&]() {
+	auto pop = [&]() {
 		char temp = traceStack.top();
 		traceStack.pop();
 		return temp;
 	};
 	//Push onto traceStack
-	void push = [&](char c) { traceStack.push(c); };
+	auto push = [&](char c) { traceStack.push(c); };
 	traceStack.push('$');
 	traceStack.push(firstState);
 	for(int i=0; i<word.length(); i++) {
 		//If a letter is not in the language, immedietly reject
-		if(terminals.find(word[i]) == terminals.end())
-			return "Invalid";
+		if(terminals.find(word[i]) == terminals.end()) {
+			cout << "Invalid because " << word[i] << " is not in the language." << endl;
+			return false;
+		}
 		top=pop();
 		mapIt = states.find(top);
 		//Keep looking until a terminal is popped
 		while(mapIt != states.end()) {
 			state = states.at(top);
 			string str = parsingTable[state][terminals.at(word[i])];
-			if(str.empty()) { return "Invalid"; }
+			if(str.empty()) {
+				cout << "Invalid at entry " << state*cols+terminals.at(word[i]) << endl; 
+				return false;
+			}
 			//Using '\n' to be lambda:
 			else if(str.size() == 1 && str[0] == '\n') {
 				top=pop();
@@ -56,14 +62,22 @@ string traceWord(string word, string& parsingTable, char firstState, map<char,in
 			mapIt = states.find(top);
 		}
 		if(word[i] == top) {
-			if(word[i] == '$')
-				return "Valid.";
 			cout << "Matched " << word[i] << ". Stack:" << printStack(traceStack) << endl;
-		else return "Invalid.";
+			if(word[i] == '$') {
+				cout << "Valid." << endl;
+				return true;
+			}
+		}
+		else {
+			cout << "Invalid match:" << word[i] << "!=" << top << " at entry " << state*cols+terminals.at(word[i]) << endl;
+			return false;
+		}
 	}
+	return false;
 }
 
 int main() {
+	cout << "Problem 1" << endl << "----------" << endl;
 	string parsingTable[5][8] = {
 		{ "TQ", "", "", "", "", "TQ", "", "" },
 		{ "", "+TQ", "-TQ", "", "", "", "\n", "\n" }, //Using '\n' = lambda
@@ -72,6 +86,12 @@ int main() {
 		{ "a", "", "", "", "", "(E)", "", "" },
 	};
 	map<char,int> parseRow = { {'E',0}, {'Q',1}, {'T',2}, {'R',3}, {'F',4} };
-	map<char,int> parseCol = { {'a',0}, {'+',1}, {'-',3}, {'*',4}, {'/',5}, {'(',6}, {')',7}, {'$',8} };
+	map<char,int> parseCol = { {'a',0}, {'+',1}, {'-',2}, {'*',3}, {'/',4}, {'(',5}, {')',6}, {'$',7} };
+	cout << "(a+a)*a$" << endl;
+	traceWord("(a+a)*a$", parsingTable, 'E', parseRow, parseCol);
+	cout << endl << "a*(a/a)$" << endl;
+	traceWord("a*(a/a)$", parsingTable, 'E', parseRow, parseCol);
+	cout << endl << "a(a+a)$" << endl;
+	traceWord("a(a+a)$", parsingTable, 'E', parseRow, parseCol);
 	return 0;
 }
